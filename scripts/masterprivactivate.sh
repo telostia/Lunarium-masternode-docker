ea3d0f70e27c424b439ed9286722860393ac5b4f
Truong Chanh Tran <zheninfy@gmail.com>
2018-05-30 10:17:55 +1000

    fixing mn docker 3

--- scripts/masterprivactivate.sh
+++ scripts/masterprivactivate.sh
@@ -4,31 +4,31 @@ while inotifywait -e modify /var/www/masternodeprivkey/masternodeprivkey.txt; do
   USERNAME=$(pwgen -s 16 1)
   PASSWORD=$(pwgen -s 64 1)
   MASTERNODEPRIVKEY=$(</var/www/masternodeprivkey/masternodeprivkey.txt)
-  echo "rpcuser=$USERNAME" >/root/solarium.conf
-  echo "rpcpassword=$PASSWORD" >>/root/solarium.conf
-  echo "server=1" >>/root/solarium.conf
-  echo "listen=1" >>/root/solarium.conf
-  echo "port=4848" >>/root/solarium.conf
-  echo "rpcport=4141" >>/root/solarium.conf
-  echo "addnode=80.211.30.202" >>/root/solarium.conf
-  echo "addnode=195.181.216.245" >>/root/solarium.conf
-  echo "addnode=217.61.106.97" >>/root/solarium.conf
-  echo "addnode=159.89.127.38" >>/root/solarium.conf
-  echo "addnode=159.203.26.237" >>/root/solarium.conf
-  echo "addnode=167.99.148.87" >>/root/solarium.conf
-  echo "addnode=159.65.9.95" >>/root/solarium.conf
-  echo "addnode=167.99.44.218" >>/root/solarium.conf
-  echo "addnode=178.62.89.167" >>/root/solarium.conf
-  echo "addnode=159.89.29.101" >>/root/solarium.conf
-  echo "maxconnections=16" >>/root/solarium.conf
-  echo "masternodeprivkey=$MASTERNODEPRIVKEY" >>/root/solarium.conf
-  echo "masternode=1" >>/root/solarium.conf
-  echo "masternodeaddr=$IP:4848" >>/root/solarium.conf
+  echo "rpcuser=$USERNAME" >/root/Solarium.conf
+  echo "rpcpassword=$PASSWORD" >>/root/Solarium.conf
+  echo "server=1" >>/root/Solarium.conf
+  echo "listen=1" >>/root/Solarium.conf
+  echo "port=4848" >>/root/Solarium.conf
+  echo "rpcport=4141" >>/root/Solarium.conf
+  echo "addnode=80.211.30.202" >>/root/Solarium.conf
+  echo "addnode=195.181.216.245" >>/root/Solarium.conf
+  echo "addnode=217.61.106.97" >>/root/Solarium.conf
+  echo "addnode=159.89.127.38" >>/root/Solarium.conf
+  echo "addnode=159.203.26.237" >>/root/Solarium.conf
+  echo "addnode=167.99.148.87" >>/root/Solarium.conf
+  echo "addnode=159.65.9.95" >>/root/Solarium.conf
+  echo "addnode=167.99.44.218" >>/root/Solarium.conf
+  echo "addnode=178.62.89.167" >>/root/Solarium.conf
+  echo "addnode=159.89.29.101" >>/root/Solarium.conf
+  echo "maxconnections=16" >>/root/Solarium.conf
+  echo "masternodeprivkey=$MASTERNODEPRIVKEY" >>/root/Solarium.conf
+  echo "masternode=1" >>/root/Solarium.conf
+  echo "masternodeaddr=$IP:4848" >>/root/Solarium.conf
   docker run -d --name solariummasternode solariummasternode
-  docker cp /root/solarium.conf solariummasternode:/root/.solarium/solarium.conf
+  docker cp /root/Solarium.conf solariummasternode:/root/.Solarium/Solarium.conf
   docker commit solariummasternode solariummasternode
   docker container rm solariummasternode
-  docker run -d --restart always -p 4848:4848 --name solariummasternode solariummasternode /root/.Solarium/Solariumd -datadir=/root/.Solarium/ -conf=/root/.Solarium/solarium.conf
+  docker run -d --restart always -p 4848:4848 --name solariummasternode solariummasternode /root/.Solarium/Solariumd -datadir=/root/.Solarium -conf=/root/.Solarium/Solarium.conf
   #cd /root/chainfile
   #docker cp ./ solariummasternode:/root/.Solarium/
   #docker stop solariummasternode
