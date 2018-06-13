18df628d1f2ae9b8388a3eb8de4874acc0578c5d
Truong Chanh Tran <zheninfy@gmail.com>
2018-06-14 04:42:26 +1000

    added docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,29 @@
+#Create 2 docker containers, 1 for solariumcoind non master node, 1 for solariumcoind masternode
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt -y -o Acquire::ForceIPv4=true update
+apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+apt -y install apt-transport-https ca-certificates curl software-properties-common git
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt -y -o Acquire::ForceIPv4=true update
+apt -y install docker-ce
+systemctl start docker
+systemctl enable docker
+docker pull ubuntu
+
+ufw default allow outgoing
+ufw default deny incoming
+ufw allow ssh/tcp
+ufw limit ssh/tcp
+ufw allow 4848/tcp
+ufw allow 4141/tcp
+ufw logging on
+ufw --force enable
+
+apt -y install fail2ban
+systemctl enable fail2ban
+systemctl start fail2ban
+
